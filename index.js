dd5179c0dad736dfc39bbf219f315e9d42d8790c
amolpradeshi <pardeshiharshal90@gmail.com>
2022-12-22 02:33:46 +0530

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require(`dotenv`).config({ debug: true });
 const mongooseConnect = require("./config/db.config");
 const eventsRouter = require("./routers/Events.routes");
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/event", eventsRouter);
